fa03a9c11782a4c22e1cc32b019a0e12ba9ec4ac
Travis Horn <travis@travishorn.com>
2019-08-23 09:59:45 -0500

    Add robust logging

--- src/index.js
+++ src/index.js
@@ -1,57 +1,94 @@
 const io = require("socket.io")();
 const randomColor = require("randomcolor");
+
+const logger = require("./logger");
 const animal = require("./animal");
 
 const port = process.env.PORT || 3000;
 
 const clientDisconnected = reason => {
-  console.log("Client disconnected.", reason);
+  logger.debug({ message: "Client disconnected.", reason });
+};
+
+const logMove = (nickname, direction, newLocation) => {
+  logger.debug({ message: "Client move", nickname, direction, newLocation });
+};
+
+const logMoveDenied = nickname => {
+  logger.debug({
+    message: "Client move denied",
+    nickname: nickname,
+    reason: "On edge of map"
+  });
 };
 
 const clientConnected = client => {
   const move = direction => {
-    console.log(`${client.nickname} wants to move ${direction}.`);
+    logger.debug({
+      message: "Client move request",
+      nickname: client.nickname,
+      direction
+    });
+
     switch (direction) {
       case "up":
         if (client.coordinates.y - 10 >= 0) {
           client.coordinates.y -= 10;
           client.emit("setLocation", client.coordinates);
-          console.log(`${client.nickname} has room and moves ${direction}.`);
+          logMove(client.nickname, direction, client.coordinates);
+        } else {
+          logMoveDenied(client.nickname);
         }
         break;
       case "right":
         if (client.coordinates.x + 10 <= 500) {
           client.coordinates.x += 10;
           client.emit("setLocation", client.coordinates);
-          console.log(`${client.nickname} has room and moves ${direction}.`);
+          logMove(client.nickname, direction, client.coordinates);
+        } else {
+          logMoveDenied(client.nickname);
         }
         break;
       case "down":
         if (client.coordinates.y + 10 <= 500) {
           client.coordinates.y += 10;
           client.emit("setLocation", client.coordinates);
-          console.log(`${client.nickname} has room and moves ${direction}.`);
+          logMove(client.nickname, direction, client.coordinates);
+        } else {
+          logMoveDenied(client.nickname);
         }
         break;
       case "left":
         if (client.coordinates.x - 10 >= 0) {
           client.coordinates.x -= 10;
           client.emit("setLocation", client.coordinates);
-          console.log(`${client.nickname} has room and moves ${direction}.`);
+          logMove(client.nickname, direction, client.coordinates);
+        } else {
+          logMoveDenied(client.nickname);
         }
     }
   };
 
-  console.log("Client connected.", client.id);
+  logger.debug({
+    message: "Client connected.",
+    clientId: client.id
+  });
 
   client.nickname = animal.random();
   client.coordinates = { x: 250, y: 250 };
   client.color = randomColor();
 
-  console.log(`${client.id} gets nickname ${client.nickname}`);
   client.emit("setNickname", client.nickname);
   client.emit("setLocation", client.coordinates);
   client.emit("setColor", client.color);
+
+  logger.debug({
+    message: "Client initialized",
+    clientId: client.id,
+    nickname: client.nickname,
+    location: client.coordinates,
+    color: client.color
+  });
 
   client.on("move", move);
   client.on("disconnect", clientDisconnected);
@@ -60,4 +97,4 @@ const clientConnected = client => {
 io.on("connection", clientConnected);
 
 io.listen(port);
-console.log(`Socket 2D Server listening on port ${port}.`);
+logger.info(`Socket 2D Server listening on port ${port}.`);
---
+++ src/logger.js
@@ -0,0 +1,8 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "debug",
+  transports: [new winston.transports.Console()]
+});
+
+module.exports = logger;
